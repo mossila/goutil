b806108bf979395b818b9ea37da1523f9eca1d63
mossila <suteanfx@gmail.com>
2015-10-02 16:55:54 +0700

    Create readlines.go

---
+++ readlines.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+//Readlns from file result in chan
+/**
+for s := range Readlns(filename) {
+    fmt.Println(s)
+}
+*/
+func Readlns(path string) chan string {
+	outChan := make(chan string)
+	go func() {
+		inFile, err := os.Open(path)
+		defer func() {
+			inFile.Close()
+			close(outChan)
+		}()
+		if err != nil {
+			log.Fatal(err)
+		}
+		scanner := bufio.NewScanner(inFile)
+		for scanner.Scan() {
+			outChan <- scanner.Text()
+		}
+	}()
+	return outChan
+}
+func main() {
+	filename := "test.txt"
+	for s := range Readlns(filename) {
+		fmt.Println(s)
+	}
+}
